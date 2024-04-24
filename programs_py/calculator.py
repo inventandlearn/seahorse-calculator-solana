a4c589f6e8b86cf789461187faa82cb273b457a1
invent2learn <alteonrene@gmail.com>
2024-04-24 10:43:07 -0400

    first commit

---
+++ programs_py/calculator.py
@@ -0,0 +1,48 @@
+
+from seahorse.prelude import *
+
+declare_id('DxqePB7M5nbSaqrru6LeEaEwh4ridSNZANt3yiuxQfSj')
+
+class Operation(Enum):
+  ADD = 0
+  SUB = 1
+  MUL = 2
+  DIV = 3
+
+class Calculator(Account):
+  owner: Pubkey
+  display: i64
+
+@instruction
+def init_calculator(owner: Signer, calculator: Empty[Calculator]):
+  calculator = calculator.init(
+    payer = owner,
+    seeds = ['Calculator', owner]
+  )
+  calculator.owner = owner.key()
+
+@instruction
+def reset_calculator(owner: Signer, calculator: Calculator):
+  print(owner.key(), 'is resetting', calculator.key())
+
+  # Verify owner
+  assert owner.key() == calculator.owner, 'This is not your calculator!'
+
+  calculator.display = 0
+
+@instruction
+def do_operation(owner: Signer, calculator: Calculator, op: Operation, num: i64):
+  # Verify owner, like before
+  assert owner.key() == calculator.owner, 'This is not your calculator!'
+
+  if op == Operation.ADD:
+    calculator.display += num
+  elif op == Operation.SUB:
+    calculator.display -= num
+  elif op == Operation.MUL:
+    calculator.display *= num
+  elif op == Operation.DIV:
+    calculator.display //= num
+
+  print(calculator.display)
+
---
+++ tests/calculator.ts
@@ -0,0 +1,72 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program, web3, BN } from "@coral-xyz/anchor";
+import { Calculator } from "../target/types/calculator";
+import { assert } from "chai";
+
+
+describe("Solana Calculator Program:", () => {
+  // Configure the client to use the local cluster.
+
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Calculator as Program<Calculator>;
+  const owner = provider.wallet.publicKey
+  const calculator = web3.PublicKey.findProgramAddressSync(
+    [Buffer.from('Calculator'), owner.toBuffer()],
+    program.programId
+  )[0]
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+
+    const tx = await program.methods.initCalculator().rpc();
+    console.log("Your transaction signature", tx);
+  })
+
+
+  it('Does some operations', async () => {
+    const add2 = await program.methods
+      .doOperation({ add : true  }, new BN(5))
+      .accounts({ owner, calculator })
+      .instruction()
+
+
+    const mul3 = await program.methods
+      .doOperation({ mul: true }, new BN(4))
+      .accounts({ owner, calculator })
+      .instruction()
+
+
+    const sub1 = await program.methods
+      .doOperation({ sub: true }, new BN(2))
+      .accounts({ owner, calculator })
+      .instruction()
+
+    const tx = new web3.Transaction()
+    tx.add(add2, mul3, sub1)
+    await provider.sendAndConfirm(tx)
+  })
+
+  // Make sure our calculator is secure
+
+
+  it('Prevents fraudulent transactions', async () => {
+    let hackerman = new web3.Keypair()
+
+    let shouldFail = await program.methods
+      .resetCalculator()
+      .accounts({
+        owner: hackerman.publicKey,
+        calculator,
+      })
+      .instruction()
+
+    let tx = new web3.Transaction()
+    tx.add(shouldFail)
+    await provider
+      .sendAndConfirm(tx, [hackerman])
+      .then(() => assert.ok(false)) // Error on success, we want a failure
+      .catch(() => {})
+  })
+});
